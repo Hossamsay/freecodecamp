dde06debf0412748c9f0c51b8f4c067d9af6f47f
Glitch (serious-carbon-d7cw5dj4f) <none>
2020-01-26 10:15:50 +0000

    ⏳🙋 Updated with Glitch

--- server.js
+++ server.js
@@ -1,102 +1,26 @@
-// init project
 var express = require('express');
-var path = require('path');
+var timeHandler = require('./timeHandler.js');
 var app = express();
+var sassMiddleware = require("node-sass-middleware");
+app.use(sassMiddleware({
+    src: __dirname + '/public',
+    dest: '/tmp'
+}));
 
-//Use our public files
-app.use(express.static('public'));
+    //'/tmp' folder holds temporary sass file
+app.use(express.static('/tmp'));
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname + '/views/index.html');
+    // http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {    
+    response.sendFile(__dirname + '/public/index.html');
 });
 
-//Returns true if the input is a number
-var isNumeric = function(input) {
-  return !isNaN(parseFloat(input)) && isFinite(input);
-}
-
-//Returns true if the given month is a month
-var validMonth = function(month) {
-  switch(month.toLower())
-      {
-    case "january":
-      break;
-    case "february":
-      break;
-    case "march":
-      break;
-    case "april":
-      break;
-    case "may":
-      break;
-    case "june":
-      break;
-    case "july":
-      break;
-    case "august":
-      break;
-    case "september":
-      break;
-    case "october":
-      break;
-    case "november":
-      break;
-    case "december":
-      break;
-  }
-}
-
-//Returns true if the format is unix, false if natural, and undefined if neither
-var isUnixTimestamp = function(time)
-{
-  if(isNumeric(time))
-  {
-    return true;
-  }
-  //Check date?
-  return false;
-}
-
-//Given a date or unix time, returns an object with both
-var convertTime = function(time)
-{
-  var isUnix = isUnixTimestamp(time);
-  if(isUnix === undefined)
-    {
-      return {unix: null, natural: null};
-    }
-  if(isUnix)
-    {
-      var date = new Date(parseInt(time));
-      console.log(date);
-      return {unix: time, natural: trimDate(date.toDateString())};
-    }
-  return  {unix: Date.parse(time), natural: time};
-}
-
-//Trims the day off the date. ex: Tuesday January 5, 1999 --> January 5, 1999
-var trimDate = function(dateStr)
-{
-  var i = 0;
-  while(i < dateStr.length && dateStr[i] !== " ")
-    {
-      i++;
-    }
-  i++;
-  return dateStr.substring(i);
-}
-
-//Handle a specific request
-app.get("/:data", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-  //valid url request?
-  if(request.params && Object.keys(request.params).length > 0)
-    {
-      response.write(JSON.stringify(convertTime(request.params.data)));
-    }
-  response.end();
+app.get("/:timeInput", function (request, response) {     
+    const cookedTime = timeHandler(request.params.timeInput);
+    response.json(cookedTime);
 });
 
-// listen for requests :)
+    // listen for requests 
 var listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + listener.address().port);
 });
