ea78b5e1d70e9f8fe3d331e98d00c5b18c17c378
Glitch (serious-carbon-d7cw5dj4f) <none>
2020-01-26 09:59:11 +0000

    🐺🌁 Updated with Glitch

--- server.js
+++ server.js
@@ -1,42 +1,102 @@
+// init project
 var express = require('express');
+var path = require('path');
 var app = express();
 
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+//Use our public files
+app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-	res.send('<p>Example usage:</p><code>http://localhost:8080/December%2015,%202015</code><br><code>http://localhost:8080/1450137600</code>');
+app.get("/", function(req, res){
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/:date([0-9]*)', function(req, res) {
-	var result = { "unix": null, "natural": null };
-	var timestamp = parseInt(req.params.date);
+//Returns true if the input is a number
+var isNumeric = function(input) {
+  return !isNaN(parseFloat(input)) && isFinite(input);
+}
 
-	var date = new Date(timestamp * 1000);
-	result.unix = timestamp;
-	result.natural = (months[date.getMonth()]) + ' ' + date.getDate() + ', ' + date.getFullYear();
+//Returns true if the given month is a month
+var validMonth = function(month) {
+  switch(month.toLower())
+      {
+    case "january":
+      break;
+    case "february":
+      break;
+    case "march":
+      break;
+    case "april":
+      break;
+    case "may":
+      break;
+    case "june":
+      break;
+    case "july":
+      break;
+    case "august":
+      break;
+    case "september":
+      break;
+    case "october":
+      break;
+    case "november":
+      break;
+    case "december":
+      break;
+  }
+}
 
-	res.send(result);
+//Returns true if the format is unix, false if natural, and undefined if neither
+var isUnixTimestamp = function(time)
+{
+  if(isNumeric(time))
+  {
+    return true;
+  }
+  //Check date?
+  return false;
+}
+
+//Given a date or unix time, returns an object with both
+var convertTime = function(time)
+{
+  var isUnix = isUnixTimestamp(time);
+  if(isUnix === undefined)
+    {
+      return {unix: null, natural: null};
+    }
+  if(isUnix)
+    {
+      var date = new Date(parseInt(time));
+      console.log(date);
+      return {unix: time, natural: trimDate(date.toDateString())};
+    }
+  return  {unix: Date.parse(time), natural: time};
+}
+
+//Trims the day off the date. ex: Tuesday January 5, 1999 --> January 5, 1999
+var trimDate = function(dateStr)
+{
+  var i = 0;
+  while(i < dateStr.length && dateStr[i] !== " ")
+    {
+      i++;
+    }
+  i++;
+  return dateStr.substring(i);
+}
+
+//Handle a specific request
+app.get("/:data", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+  //valid url request?
+  if(request.params && Object.keys(request.params).length > 0)
+    {
+      response.write(JSON.stringify(convertTime(request.params.data)));
+    }
+  response.end();
 });
 
-app.get('/:natString([a-zA-Z]*)', function(req, res) {
-	var result = { "unix": null, "natural": null };
-	var dateArr = req.params.natString.split(' ');
-
-	if (months.indexOf(dateArr[0]) > -1) {
-		var month = months.indexOf(dateArr[0]);
-		var day = dateArr[1].replace(',', '');
-		var year = dateArr[2];
-
-		var date = new Date(year, month, day);
-		result.unix = date.getTime() / 1000;
-		result.natural = (months[date.getMonth()]) + ' ' + date.getDate() + ', ' + date.getFullYear();
-
-		res.send(result);
-	}
-
-	res.send(result);
-});
-
-app.listen(8080, function() {
-	console.log('App listening on port 8080.');
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
 });
